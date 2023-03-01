6fde8728ad5d7780c661de2ce8c71b9a3d994683
sebastianvergara <s_vergara@javeriana.edu.co>
2023-03-01 02:12:00 -0500

    Se rehace el sistema de navegacion, y se hacen otros pequeños cambios.

--- app/src/main/java/com/pontimovil/hypo/MainActivity.kt
+++ app/src/main/java/com/pontimovil/hypo/MainActivity.kt
@@ -10,24 +10,31 @@ import com.pontimovil.hypo.databinding.ActivityMainBinding
 import com.pontimovil.hypo.databinding.FragmentPolaroidSnaptouchBinding
 // Import replace extension function
 import androidx.fragment.app.replace
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+
 import com.pontimovil.hypo.cameraFrames.polaroidSnaptouch
 
 // TODO: Crear fragmentos de decoraciones de camaras.
 
-private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Hide the bar
         supportActionBar?.hide()
-        window.statusBarColor = getColor(R.color.black)
-        supportFragmentManager.commit {
-            replace<polaroidSnaptouch>(binding.frameContainer.id)
-            setReorderingAllowed(true)
-            addToBackStack("replacement")
-        }
+        // Set the color of the status bar
+        window.statusBarColor = resources.getColor(R.color.black)
+        navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment)
+        setupWithNavController(binding.bottomNavigationView, navController)
+
+
+
     }
 }
