2e8e85e0465113f2853b42be16c5f083df136180
sebastianvergara <s_vergara@javeriana.edu.co>
2023-02-28 21:58:10 -0500

    Se añaden logos de polaroid, asi como la de camara 1.

--- app/src/main/java/com/pontimovil/hypo/MainActivity.kt
+++ app/src/main/java/com/pontimovil/hypo/MainActivity.kt
@@ -3,6 +3,8 @@ package com.pontimovil.hypo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// TODO: Crear fragmentos de decoraciones de camaras.
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
