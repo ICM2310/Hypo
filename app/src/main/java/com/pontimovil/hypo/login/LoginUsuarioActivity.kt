09db4f8e49925817b8eaccea6944dffa198005ec
CesarMaldonado14 <cesar-maldonado@javeriana.edu,co>
2023-04-17 12:00:05 -0500

    Correccion barra superior y binding del login

--- app/src/main/java/com/pontimovil/hypo/login/LoginUsuarioActivity.kt
+++ app/src/main/java/com/pontimovil/hypo/login/LoginUsuarioActivity.kt
@@ -1,25 +1,27 @@
 package com.pontimovil.hypo.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.pontimovil.hypo.MainActivity
-import com.pontimovil.hypo.R
 import com.pontimovil.hypo.databinding.ActivityLoginUsuarioBinding
 
-private lateinit var binding: ActivityLoginUsuarioBinding
-private lateinit var auth: FirebaseAuth;
 
 class LoginUsuarioActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginUsuarioBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ActivityLoginUsuarioBinding.inflate(layoutInflater)
+        binding = ActivityLoginUsuarioBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.hide()
 
         auth = FirebaseAuth.getInstance()
 
