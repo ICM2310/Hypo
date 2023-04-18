5a0f9f461de6f72e89a6d3ffae0e04c9b000d5a0
gabriela luigi <luigigabriela68@gmail.com>
2023-04-18 17:10:21 -0500

    Adelantos camara

--- app/src/main/java/com/pontimovil/hypo/rolls.kt
+++ app/src/main/java/com/pontimovil/hypo/rolls.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.pontimovil.hypo.databinding.FragmentRollsBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +22,7 @@ class rolls : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding: FragmentRollsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +37,12 @@ class rolls : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rolls, container, false)
+        binding = FragmentRollsBinding.inflate(layoutInflater)
+
+        binding.imagenSelector.setOnClickListener {
+            Toast.makeText(activity, "", Toast.LENGTH_SHORT).show()
+        }
+        return binding.root
     }
 
     companion object {
