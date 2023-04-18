53d2e95773e4a14d3d68761743fc13b8ff4ba4af
gabriela luigi <luigigabriela68@gmail.com>
2023-04-18 13:30:48 -0500

    Revert "seleccionar fotos adelanto"
    This reverts commit c61acd3d58c40551f0f2443a9506c33aaff3ebdd.

--- app/src/main/java/com/pontimovil/hypo/rolls.kt
+++ app/src/main/java/com/pontimovil/hypo/rolls.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import com.pontimovil.hypo.databinding.FragmentRollsBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,14 +20,12 @@ class rolls : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var binding: FragmentRollsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
-
         }
     }
 
@@ -38,10 +34,7 @@ class rolls : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding.IngresarImagen.setOnClickListener {
-            Toast.makeText(activity, "Ingresar Control", Toast.LENGTH_SHORT).show()
-        }
-        return binding.root
+        return inflater.inflate(R.layout.fragment_rolls, container, false)
     }
 
     companion object {
