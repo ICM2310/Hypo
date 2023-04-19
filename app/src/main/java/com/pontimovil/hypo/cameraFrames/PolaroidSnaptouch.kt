4b3d6caa196862ec05c254ceeb02d58f195d6c41
sebastianvergara <s_vergara@javeriana.edu.co>
2023-04-18 23:56:22 -0500

    Se termina el sistema de fotografia utilizando camerax api. Se tiene lista la interfaz para notificar para animaciones.

--- app/src/main/java/com/pontimovil/hypo/cameraFrames/PolaroidSnaptouch.kt
+++ app/src/main/java/com/pontimovil/hypo/cameraFrames/PolaroidSnaptouch.kt
@@ -6,8 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.pontimovil.hypo.R
+import androidx.camera.core.ImageCapture
+import androidx.core.app.ActivityCompat
 import com.pontimovil.hypo.databinding.FragmentPolaroidSnaptouchBinding
+import java.util.concurrent.ExecutorService
+
+import android.Manifest
+import android.content.ContentValues
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.provider.MediaStore
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executors
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.core.Preview
+import androidx.camera.core.CameraSelector
+import android.util.Log
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +41,16 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class polaroidSnaptouch : Fragment() {
+
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var cameraExecutor: ExecutorService
+    private val TAG = "Hypo"
+    private val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+
+
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    private val REQUEST_CODE_PERMISSIONS = 10
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -33,6 +64,160 @@ class polaroidSnaptouch : Fragment() {
 
     }
 
+    private class LuminosityAnalyzer(private val listener: (Any) -> Int) : ImageAnalysis.Analyzer {
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy) {
+
+            val buffer = image.planes[0].buffer
+            val data = buffer.toByteArray()
+            val pixels = data.map { it.toInt() and 0xFF }
+            val luma = pixels.average()
+
+            listener(luma)
+
+            image.close()
+        }
+    }
+
+
+    private fun startCamera() {
+        val context = requireContext()
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                        Log.v("Lumin", "Average luminosity: $luma")
+                    })
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture, imageAnalyzer)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(context))
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(), permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    activity,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                activity?.finish()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        Log.d(TAG, "takePhoto")
+        val imageAnalyzer = ImageAnalysis.Builder()
+            .build()
+            .also {
+                it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                    Log.d(TAG, "Average luminosity: $luma")
+                })
+            }
+        val imageCapture = imageCapture ?: return
+        Log.d(TAG, "imageCapture")
+        Toast.makeText(activity, "Taking photo", Toast.LENGTH_SHORT).show()
+        val context = requireContext()
+        val contentResolver = context.contentResolver
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/Hypo") //
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(context),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun
+                        onImageSaved(output: ImageCapture.OutputFileResults){
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    Log.d(TAG, msg)
+                    listener?.onPictureTaken()
+                }
+            }
+        )
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -41,7 +226,7 @@ class polaroidSnaptouch : Fragment() {
         binding = FragmentPolaroidSnaptouchBinding.inflate(layoutInflater)
 
         binding.shutter.setOnClickListener {
-            Toast.makeText(activity, "Foto!", Toast.LENGTH_SHORT).show()
+            takePhoto()
         }
 
         binding.flash.setOnClickListener {
@@ -51,7 +236,15 @@ class polaroidSnaptouch : Fragment() {
         binding.flip.setOnClickListener {
             Toast.makeText(activity, "Switch!", Toast.LENGTH_SHORT).show()
         }
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
 
+        cameraExecutor = Executors.newSingleThreadExecutor()
         return binding.root
     }
 
@@ -73,5 +266,26 @@ class polaroidSnaptouch : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    private var listener: OnPictureTakenListener? = null
+
+    interface OnPictureTakenListener {
+        fun onPictureTaken()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (parentFragment is OnPictureTakenListener) {
+            listener = parentFragment as OnPictureTakenListener
+        }
+    }
+
+
+}
+
+class LumaListener {
+    operator fun invoke(luma: Double) {
+        //Log.d("LumaListener", "Average luminosity: $luma")
     }
 }
--- app/src/main/java/com/pontimovil/hypo/rollSelectorModern.kt
+++ app/src/main/java/com/pontimovil/hypo/rollSelectorModern.kt
@@ -1,10 +1,14 @@
 package com.pontimovil.hypo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import com.pontimovil.hypo.databinding.FragmentRollSelectorModernBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,21 +24,37 @@ class rollSelectorModern : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var binding: FragmentRollSelectorModernBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = FragmentRollSelectorModernBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d("RollSelectorModern", "Creating view")
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_roll_selector_modern, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("RollSelectorModern", "View created")
+        binding.photoWidget.setOnClickListener {
+            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+        binding.imageView2.setOnClickListener {
+            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
@@ -55,5 +75,25 @@ class rollSelectorModern : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+
+
+    }
+
+    // function to udpdate the linear layout with the new roll
+    fun updateRoll(roll: Roll) {
+        Log.d("RollSelectorModern", "Updating roll")
+        val images = roll.images
+        val layout = binding.photoLayout
+        for (image in images) {
+            val imageView = ImageView(context)
+            imageView.setImageURI(image)
+            layout.addView(imageView)
+        }
     }
 }
+
+private fun ImageView.setImageURI(image: String) {
+    this.setImageURI(image)
+}
+
+
